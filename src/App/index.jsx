6741cb1b3cd687bec790121f12f221c40b1f4f1e
isaquetdiniz <itd@cin.ufpe.br>
2020-09-29 16:38:41 -0300

    (master): project setup

---
+++ src/App/index.jsx
@@ -0,0 +1,9 @@
+import { func } from 'prop-types';
+import React from 'react';
+import Map from '../components/Map';
+export default function App(){
+    return(
+        <Map>
+        </Map>
+    )
+}
---
+++ src/components/Map/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Map(){
+    return(
+        <div>Aqui é o mapa</div>
+    )
+}
